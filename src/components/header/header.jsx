1fe36f465b82971ff372e0fa305e50390bb7221b
abdalrahman <cgrovestreet@gmail.com>
2025-10-11 05:56:14 +0300

    finish nav bar and responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -16,13 +16,13 @@ function Header(){
             </nav>
 
 
-            <button onClick={() => setShowModel(true)} className="menu">show model</button>
+            <i onClick={() => setShowModel(true)} className="menu material-icons">menu</i>
 
 
             {showModel && (
                 <div className="fixed">
                     <ul className="model">
-                        <li><i onClick={() => setShowModel(false)} class="material-icons">clear</i></li>
+                        <li><i onClick={() => setShowModel(false)} className="material-icons">clear</i></li>
                         <li><a href="#!">About</a></li>
                         <li><a href="#!">Articles</a></li>
                         <li><a href="#!">Projects</a></li>
